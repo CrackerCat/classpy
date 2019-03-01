f96d86933b733a7111b4d2bf499b11ac4384ce24
zxh0 <zhangxiuhong2013@gmail.com>
2019-03-01 18:09:59 +0800

    Update FileParser2.java

--- classpy-common/src/main/java/com/github/zxh/classpy/FileParser2.java
+++ classpy-common/src/main/java/com/github/zxh/classpy/FileParser2.java
@@ -2,15 +2,24 @@ package com.github.zxh.classpy;
 
 import com.moandjiezana.toml.Toml;
 
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class FileParser2 {
 
-    public static void parse() {
+    public static void parse(byte[] data) {
         Toml spec = new Toml().read(FileParser2.class.getResourceAsStream("/java_class.toml"));
         System.out.println(spec);
+        System.out.println(spec.getString("endianness"));
+        System.out.println(spec.getString("root_node"));
     }
 
-    public static void main(String[] args) {
-        parse();
+    public static void main(String[] args) throws Exception {
+        URL classURL = FileParser2.class.getResource("/com/github/zxh/classpy/FileParser2.class");
+        byte[] classData = Files.readAllBytes(Paths.get(classURL.toURI()));
+        System.out.println(classData);
+        parse(classData);
     }
 
 }
