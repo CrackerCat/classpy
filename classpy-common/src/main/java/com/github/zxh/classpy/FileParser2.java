c20a2e555ab1ae714f04f1cc331c58299c819a01
zxh0 <zhangxiuhong2013@gmail.com>
2019-03-02 10:18:40 +0800

    FileParser2 ..

--- classpy-common/src/main/java/com/github/zxh/classpy/FileParser2.java
+++ classpy-common/src/main/java/com/github/zxh/classpy/FileParser2.java
@@ -1,5 +1,6 @@
 package com.github.zxh.classpy;
 
+import com.github.zxh.classpy.common.BytesReader;
 import com.github.zxh.classpy.spec.FileSpec;
 
 import java.net.URL;
@@ -8,19 +9,29 @@ import java.nio.file.Paths;
 
 public class FileParser2 {
 
-    public static void parse(byte[] data) {
-        FileSpec spec = new FileSpec("/java_class.toml");
+    private final FileSpec spec;
+    private final BytesReader reader;
 
+    public FileParser2(byte[] data) {
+        spec = new FileSpec("/java_class.toml");
+        reader = new BytesReader(data, spec.getByteOrder());
+    }
+
+    public void parse() {
         System.out.println(spec);
         System.out.println(spec.getByteOrder());
         System.out.println(spec.getRootNode());
+        spec.getRootNode().entrySet().forEach(e -> {
+            System.out.println(e.getKey());
+            System.out.println(e.getValue());
+        });
     }
 
     public static void main(String[] args) throws Exception {
         URL classURL = FileParser2.class.getResource("/com/github/zxh/classpy/FileParser2.class");
         byte[] classData = Files.readAllBytes(Paths.get(classURL.toURI()));
         System.out.println(classData);
-        parse(classData);
+        new FileParser2(classData).parse();
     }
 
 }
