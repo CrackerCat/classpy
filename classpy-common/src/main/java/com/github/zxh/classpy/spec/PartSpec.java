a080a3e511e20a2f62d55dad1a2c1b813975ad7d
zxh0 <zhangxiuhong2013@gmail.com>
2019-03-06 16:54:24 +0800

    +PartSpec

---
+++ classpy-common/src/main/java/com/github/zxh/classpy/spec/PartSpec.java
@@ -0,0 +1,11 @@
+package com.github.zxh.classpy.spec;
+
+// &typeName:format
+// &typeName[$lenExpr]
+public class PartSpec {
+
+    private String typeName;
+    private String format;
+    private String lenExpr;
+
+}
